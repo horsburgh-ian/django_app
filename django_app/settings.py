ccb27b0bcdeaadc6e5ae6b3b2aca6d22ce9b665e
ianhorsburgh <nickh91101@gmail.com>
2022-11-07 14:38:53 -0800

    settings

--- django_app/settings.py
+++ django_app/settings.py
@@ -107,8 +107,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "projects/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # Uncomment if you have extra static files and a directory in your GitHub repo.
 # If you don't have this directory and have this uncommented your build will fail
