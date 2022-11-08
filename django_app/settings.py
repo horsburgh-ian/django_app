44108ec8610c08ec074c92a0d3abbc51f0aed11c
ianhorsburgh <nickh91101@gmail.com>
2022-11-07 16:04:54 -0800

    static settings

--- django_app/settings.py
+++ django_app/settings.py
@@ -17,7 +17,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 #DEBUG = True
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+DEBUG = False
+#ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 #ALLOWED_HOSTS = []
 
 # Application definition
@@ -107,7 +108,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/static"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
